cd4f580383c78bb6ef052fe642da400794c6307e
Christopher Nah <christopher_nah@yahoo.com.sg>
2018-06-13 20:56:34 +0800

    Version 0.2 (Generate any waveform)
    Triangle waves, square, sawtooth (or any ratio), sine, etc.

---
+++ Python/IntelligentMusic/Alpha version/Version 0.2 (Generate any waveform)/evaluate.py
@@ -0,0 +1,126 @@
+#dataPoints is an array from 1 to how many bits there are for the note length. Previously in generate note datapoint is used; now the datapoint need to be changed to noteData[0]
+
+from __future__ import division
+import numpy as np
+import itertools
+from scipy import signal
+
+from parameters import *
+from scales import *
+
+
+#*waveforms (arrays)
+#for wave in waveforms
+
+'''
+def newInstrument():
+	global data
+	data = []
+
+	return
+'''
+			#def GenerateNote(noteLength,freq,addfreq=[]):
+			#make def evaluate which for loops the noteData array
+def GenerateNote(freqComponents,noteData):
+	"This generates a note of frequency in Hz and times of 1/64 notes"
+	print ""
+
+
+	
+	#global data 
+		##access current data
+
+	data = []
+
+	print "Current bits: " + str(len(data)) + " (" + str(len(data)/44100) + " bars)"
+		#show bits of current data
+
+	localData = "" 
+		#initialise localData
+	
+	for noteData in noteData:
+		print "Generating melody (" + str(note(str(noteData[1]))) + "Hz) " + "(length: " + str(noteData[0]) + ")..."
+
+		bitsOfNote = ((noteData[0]/64)*bitsPerBeat)//1 
+		#// by 1 so that bitsOfNote is an integer
+		#(how much of a beat) then * bitsPB 
+		####change note length to 32 later and generate 1 second
+	
+		dataPoints = np.arange(0,bitsOfNote)/44100 
+		#i.e. how much of the waveform to generate; array of what point of wave to generate
+		#if bitsOfNote is 44100, from 0 to 44099, 44100 values
+		#e.g. bitsOfNote is 22050, at the end, datapoint = 0.5				#divided by 44100 so sampling rate is preserved; 44100 values
+		
+		dataPoints = dataPoints*(2*np.pi)
+		#e.g. end dataPoint is 0.5, dataPoint will be (2pi)/2 (half wave)
+
+		theFreq = note(str(noteData[1])) 
+			#given frequency
+		freqRatio = theFreq/freqComponents[0][0]
+		
+		for waveform in freqComponents:
+			finalFreq = freqRatio*waveform[0]
+
+			print "		Generating waveform (" + str(waveform[1]) + ", " + str(finalFreq) + "Hz)..."
+			
+			if waveform[1] == "sine":
+			#addData refers to additional data
+				addData = np.sin(dataPoints*finalFreq)
+		###MIGHT BE WRONG note data??? Need to divide by base freq.
+				#make array; DO NOT Append to current data
+				#adddata to not overwrite original data
+			elif waveform[1] == "square":
+				addData = signal.square(dataPoints*finalFreq)
+				#square
+			elif waveform[1] == "sawtooth":
+				addData = signal.sawtooth(dataPoints*finalFreq)
+				#sawtooth
+			elif waveform[1] == "triangle":
+				addData = signal.sawtooth((dataPoints*finalFreq),0.5)
+				#triangle
+			if localData == "":
+				localData = addData
+			else:
+				localData = [x + y for x, y in zip(addData, localData)]
+				#add frequencies and append to original data
+
+		data = np.append(data, localData)
+			#add to original data
+		print "Completed bits after generation: " + str(len(data)) + " (" + str(len(data)/44100) + " bars)"
+
+
+		print ""
+
+	global song
+	song = [x + y for x, y in list(itertools.izip_longest(song, data, fillvalue=0))]
+	
+	return
+
+
+
+	'''
+
+	####Additional Frequencies####
+	freqNo = 1
+	for addfreq in addfreq:
+		print "Combining with additional frequency " + str(freqNo) + " (" + str(addfreq) + " Hz)..."
+		adddata = np.array(np.sin(dataPoints*(addfreq*(2*np.pi))))
+			#adddata to not overwrite original data
+		localData = [x + y for x, y in zip(adddata, localData)]
+			#add frequencies and append to original data
+		freqNo += 1
+	####Additional Frequencies####
+	
+	'''
+
+	
+'''
+def addInstrumentToSong():
+	global song
+	song = [x + y for x, y in list(itertools.izip_longest(song, data, fillvalue=0))]
+	
+	global totalBitsOfNote
+	totalBitsOfNote = len(song)
+		#MUST BE SAME AS x FOR PLOTTING
+	return
+'''
