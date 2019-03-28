a20841bdfc24667be3661808311adac69ac234ad
Christopher Nah <christopher_nah@yahoo.com.sg>
2019-03-28 12:32:56 +0800

    Made new version 1 folder

---
+++ Python/IntelligentMusic/Version 1.0.0 - IntelligentMusic 2019 April/Future/Test File .md
@@ -0,0 +1,37 @@
+# Level 1
+Global project
+## Level 2
+Idea
+
+### Level 3
+Sub-component
+
+#### Level 4?
+Generate waveform
+
+##### Level 5?
+Volume of waves
+* RMS value is the relative value of the waves. (Power of wave)
+* For square wave vs triangle/ sawtooth wave is 2:1 accordingly due to area.
+
+###### Level 6?
+test
+
+####### Level 7?
+test
+
+### test
+
+```
+Text
+```
+
+# test
+
+* [Google](http://www.google.com.sg/) - Links
+
+
+# People
+* **Crispy** - *IntelligentMusic* - [Test](https://github.com/IntelligentMusic)
+
+* test
---
+++ Python/IntelligentMusic/Version 1.0.0 - IntelligentMusic 2019 April/parameters.py
@@ -0,0 +1,37 @@
+from __future__ import division
+
+####Set Variables Here####
+bpm = 60
+
+#plotting
+toPlot = 1 #1 to plot; 0 to not plot
+plotBars = 0 #0 for one wave. 0 and len(song) in totalBitsOfNotes for total song length. Divide by bitrate to plot specific bits. Divide by frequency to plot specific waves.
+plotOffsetBars = 0
+toPlotBefore = 0 #1 to plot plotBars BEFORE the plotOffsetBars, 0 for only AFTER
+normalise = 1 #1 to normalise; 0 to retain original amplitude
+
+#exporting
+toExport = 1 #1 to export; 0 to not export
+outputFileName = "volume&phaseshift"
+
+maxLoud = 0.5 #loudness from 0 to 1 
+##to be applied at the end as normalisation i.e. data * maxLoud 
+####Variables####
+
+
+
+
+
+####Calculations####
+#data = []
+song = []
+bitsPerBeat = 44100/(bpm/60)
+	#3 beats per second
+	#bps is bpm/60 
+	#bitsPB is bitrate (44100) / bps
+	#to obtain length (in fraction of 44100=1 second)
+plotBars = plotBars * bitsPerBeat
+plotOffsetBars = plotOffsetBars * bitsPerBeat
+bigOffsetDataPoint = plotOffsetBars+plotBars
+smallOffsetDataPoint = plotOffsetBars-plotBars
+####Calculations####
